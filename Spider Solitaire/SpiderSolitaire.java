cc2bf2fdf80ec7248343275cdbc0fa072d811ade
Sumit Mantri <92963348+SumitM123@users.noreply.github.com>
2025-04-09 19:17:07 -0700

    Update SpiderSolitaire.java

--- Spider Solitaire/SpiderSolitaire.java
+++ Spider Solitaire/SpiderSolitaire.java
@@ -1,21 +1,3 @@
-/* Reflection Activity 6
- * Sumit Mantri; seventh period APCS; December 12, 2020 
- * Activity 6 took me around an hour to finish.
- * I would say activity 6 wasn't too hard to do. It was essentially just using try-catch inside the if-else-if statements of the commands to catch the input
- * errors of the user, print out the error, and then continue on with the game. The only hard part was figuring out where to use the try-catch statements. At
- * first, I was using these statements inside the Board class on the methods that I've worked on. Then, I realized that the errors that are going to occur are
- * user input errors where the type in an invalid command, or an invalid card, etc. After realizing this, I used the try-catch statements in Spider Solitaire
- * where user input is present. 
- */
-/* Reflection Activity 7
- * Sumit Mantri; seventh period APCS; December 18, 2020 
- * Activity 7 took me around two and half hours to finish.
- * This activity was pretty difficult, especially the loading part of the activity. When I was converting the decks to String, it wasn't all too hard, but I
- * made it quite sofisticated has all the decks are in one line, and the drawpile in another, so I wouldn't know where each deck starts and ends. I didn't
- * realize it was complicated, until trying to do the loading part of the activity. After thinking about it for a while to make it simplier for me to load 
- * the game, I made it so that each deck takes one line with the drawpile being the last deck of them all. After doing so, everything came quite easy for me 
- * has I just need to read each line for a new deck. 
- */
 import java.util.*;
 import java.io.IOException;
 public class SpiderSolitaire
@@ -57,7 +39,6 @@ public class SpiderSolitaire
             String command = input.next();
 
             if (command.equals("move")) {
-                /* *** TO BE MODIFIED IN ACTIVITY 5 *** */
                 try {
                     String symbol = input.next();
                     int sourceStack = input.nextInt();
@@ -71,7 +52,6 @@ public class SpiderSolitaire
                 board.drawCards();
             }
             else if (command.equals("clear")) {
-                /* *** TO BE MODIFIED IN ACTIVITY 5 *** */
                 try {
                     int sourceStack = input.nextInt();
                     board.clear(sourceStack - 1);
